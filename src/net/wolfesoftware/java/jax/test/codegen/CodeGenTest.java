cfeae8abedca7637a74da5b1f2af7c69e05cd533
Josh Wolfe <thejoshwolfe@gmail.com>
2009-10-02 03:48:40 -0700

    TryCatch works

--- src/net/wolfesoftware/java/jax/test/codegen/CodeGenTest.java
+++ src/net/wolfesoftware/java/jax/test/codegen/CodeGenTest.java
@@ -21,15 +21,15 @@ public class CodeGenTest
     private static final int mode = 1*RUN | 1*CLEAN | 1*VERBOSE;
     private static final String dir = "test/codegen";
     private static final String[] tests = {
-//        "primitive/Arithmetic",
-//        "primitive/BooleanType",
-//        "primitive/ConstReturn",
-//        "primitive/VoidFunction",
-//        "primitive/LocalVariables",
-//        "primitive/IfThenElse",
-//        "reference/StringLiteral",
-//        "reference/FunctionInvocation",
-//        "reference/RuntimeType",
+        "primitive/Arithmetic",
+        "primitive/BooleanType",
+        "primitive/ConstReturn",
+        "primitive/VoidFunction",
+        "primitive/LocalVariables",
+        "primitive/IfThenElse",
+        "reference/StringLiteral",
+        "reference/FunctionInvocation",
+        "reference/RuntimeType",
         "exceptions/TryCatch",
     };
 
