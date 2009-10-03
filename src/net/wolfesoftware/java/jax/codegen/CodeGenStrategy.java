adda88b714f6d4c7dac951b9873c79c3bf62a1e5
Josh Wolfe <thejoshwolfe@gmail.com>
2009-10-02 23:38:02 -0700

    got rid of stupid Strategy pattern

--- src/net/wolfesoftware/java/jax/codegen/CodeGenStrategy.java
+++
@@ -1,9 +0,0 @@
-package net.wolfesoftware.java.jax.codegen;
-
-import java.io.FileNotFoundException;
-import net.wolfesoftware.java.jax.ast.Root;
-
-public abstract class CodeGenStrategy
-{
-    public abstract void generateCode(Root root, String outputFilename) throws FileNotFoundException;
-}
