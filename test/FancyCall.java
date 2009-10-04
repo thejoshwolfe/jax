b152e610bffdf8e8cbbab3c27379f4f484a77306
Josh Wolfe <thejoshwolfe@gmail.com>
2009-10-03 17:25:20 -0700

    updated Fancy test to show that we can read negative numbers now

--- test/FancyCall.java
+++ test/FancyCall.java
@@ -2,6 +2,10 @@ public class FancyCall
 {
     public static void main(String[] args)
     {
-        Fancy.printThis("+++ PASS");
+        boolean pass = true;
+        pass &= Fancy.readFirstDigits("abc123efg456") == 123;
+        pass &= Fancy.readFirstDigits("no digits here") == -1;
+        if (pass)
+            Fancy.printThis("+++ PASS");
     }
 }
