e4928a9e494e75873014de30fd0727a9d2dd303a
Josh Wolfe <thejoshwolfe@gmail.com>
2009-10-06 04:46:51 -0700

    fancy test all works now

--- test/FancyCall.java
+++ test/FancyCall.java
@@ -5,6 +5,9 @@ public class FancyCall
         boolean pass = true;
         pass &= Fancy.readFirstDigits("abc123efg456") == 123;
         pass &= Fancy.readFirstDigits("no digits here") == -1;
+        pass &= Fancy.dareYouToParseThisInt("1234").equals("1234");
+        pass &= Fancy.dareYouToParseThisInt("not a number").equals("Nice Try!\n");
+        pass &= Fancy.meaningOfLife() == 42;
         if (pass)
             Fancy.printThis("+++ PASS");
     }
