c6312b5c9615800d0bbdb5c87c54bc127a4e4cdd
Josh Wolfe <thejoshwolfe@gmail.com>
2009-10-11 22:22:34 -0700

    beefed up the ArrayDereference testcase

--- test/arrays/ArrayDereferenceCall.java
+++ test/arrays/ArrayDereferenceCall.java
@@ -3,7 +3,11 @@ public class ArrayDereferenceCall
     public static void main(String[] args)
     {
         String[] arr = new String[] { "first", "second" };
-        if (ArrayDereference.secondOne(arr) == "second")
+        int[][][] arr2 = {null, null, null, null, {null, null, {1234}}};
+        boolean pass =
+            ArrayDereference.secondOne(arr) == "second" ||
+            ArrayDereference.fifthThird(arr2)[0] == 1234;
+        if (pass)
             System.out.println("+++ PASS");
         else
             System.out.println("*** FAIL");
