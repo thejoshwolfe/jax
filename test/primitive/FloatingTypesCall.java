a1c338c4515ea8ead8bf573ad06a38beaf876335
Josh Wolfe <thejoshwolfe@gmail.com>
2009-10-18 22:03:08 -0700

    beefed up the test case for FloatingTypes

--- test/primitive/FloatingTypesCall.java
+++ test/primitive/FloatingTypesCall.java
@@ -3,7 +3,9 @@ public class FloatingTypesCall
     public static void main(String[] args)
     {
         if (FloatingTypes._3_14f() - 3.14f == 0.0f &&
-            FloatingTypes._3_14() - 3.14 == 0.0)
+            FloatingTypes._3_14() - 3.14 == 0.0 &&
+            FloatingTypes.gimmeThatFloat(1.5f) - 1.5f == 0.0 &&
+            FloatingTypes.gimmeThatDouble(1.3) - 1.3 == 0.0)
             System.out.println("+++ PASS");
         else
             System.out.println("*** FAIL");
