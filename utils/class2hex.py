7208464808350d19b61b9332a356b0bf203fc856
Josh Wolfe <thejoshwolfe@gmail.com>
2009-11-20 00:30:22 -0700

    disassembler can do fields

--- utils/class2hex.py
+++ utils/class2hex.py
@@ -13,10 +13,23 @@ CONSTANT_Methodref = 10
 CONSTANT_InterfaceMethodref = 11
 CONSTANT_NameAndType = 12
 
+ACC_PUBLIC = 0x0001
+ACC_PRIVATE = 0x0002
+ACC_PROTECTED = 0x0002
+ACC_STATIC = 0x0008
+ACC_FINAL = 0x0010
+ACC_SUPER = ACC_SYNCHRONIZED = 0x0020
+ACC_VOLATILE = 0x0040
+ACC_TRANSIENT = 0x0080
+ACC_NATIVE = 0x0100
+ACC_INTERFACE = 0x0200
+ACC_ABSTRACT = 0x0400
+ACC_STRICT = 0x0800
+
 def main(input, output):
     """
-    input : str - such as open(filename, "r").read()
-    output : file - such as stdout
+    input : str - such as open("Example.class", "r").read()
+    output : file - such as stdout or anything with a method write(str)
     return : str - error message or None
     """
     
@@ -174,7 +187,7 @@ def main(input, output):
             assert(constantPool[nameAndTypeIndex][0] == CONSTANT_NameAndType)
             className = constantPool[constantPool[classIndex][1]][1]
             nameAndTypeIndexes = constantPool[nameAndTypeIndex][1:]
-            nameAndType = constantPool[nameAndTypeIndexes[0]][1] + constantPool[nameAndTypeIndexes[1]][1]
+            nameAndType = constantPool[nameAndTypeIndexes[0]][1] + ":" + constantPool[nameAndTypeIndexes[1]][1]
             constantTypeName = {
                 CONSTANT_Fieldref:"Fieldref", 
                 CONSTANT_Methodref:"Methodref", 
@@ -188,13 +201,88 @@ def main(input, output):
             assert(constantPool[descriptorIndex][0] == CONSTANT_Utf8)
             name = constantPool[nameIndex][1]
             descriptor = constantPool[descriptorIndex][1]
-            output.write("NameAndType = %s%s\n" % (name, descriptor))
+            output.write("NameAndType = %s:%s\n" % (name, descriptor))
             i += 1
         else:
-            return "omgwtf!ddd!!"
+            return "omgwtf!!!"
         output.write("".join("\t%s\n" % hexStr for hexStr in hexStrs) + "\t\n")
     
     # class declaration
+    (h1, accessFlags) = readShort()
+    output.write("; ")
+    if accessFlags & ACC_PUBLIC:
+        output.write("public ")
+    if accessFlags & ACC_FINAL:
+        output.write("final ")
+    if accessFlags & ACC_INTERFACE:
+        output.write("interface ")
+    else:
+        if accessFlags & ACC_ABSTRACT:
+            output.write("abstract ")
+        output.write("class ")
+    (h2, classIndex) = readShort()
+    output.write(constantPool[constantPool[classIndex][1]][1] + " ")
+    (h3, superClassIndex) = readShort()
+    output.write("extends " + constantPool[constantPool[superClassIndex][1]][1])
+    # interfaces
+    (h4, interfaceCount) = readShort()
+    interfacesHexStr = ""
+    if interfaceCount != 0:
+        output.write(" implements ")
+        interfaceNames = []
+        for i in range(interfaceCount):
+            (h, classIndex) = readShort()
+            interfaceNames.append(constantPool[constantPool[classIndex][1]][1])
+            interfacesHexStr += "\t" + h + "\n"
+        output.write(", ".join(interfaceNames))
+    output.write(("\n" + "%s\n" * 4 + "%s\n") % (h1, h2, h3, h4, interfacesHexStr))
+
+    # function for reading attributes on fields, methods, and at the end of the class
+    def readAttribute(indentation):
+        (h1, nameIndex) = readShort()
+        name = constantPool[nameIndex][1]
+        (h2, length) = readInt()
+        output.write("%s; attribute \"%s\"; size=%i\n" % (indentation, name, length))
+        output.write(((indentation + "%s\n") * 2) % (h1, h2))
+        if False:
+            pass
+        else:
+            # unknown attribute name
+            (h, _) = readString(length)
+            output.write(indentation + "\t" + h)
+        output.write(indentation + "\n")
+    
+    # fields
+    (h, fieldCount) = readShort()
+    output.write("; fields; size=%i\n" % fieldCount)
+    output.write(h + "\n")
+    for i in range(fieldCount):
+        (h1, accessFlags) = readShort()
+        (h2, nameIndex) = readShort()
+        (h3, descriptorIndex) = readShort()
+        output.write("\t; ")
+        if accessFlags & ACC_PUBLIC:
+            output.write("public ")
+        elif accessFlags & ACC_PRIVATE:
+            output.write("private ")
+        elif accessFlags & ACC_PROTECTED:
+            output.write("protected ")
+        if accessFlags & ACC_STATIC:
+            output.write("static ")
+        if accessFlags & ACC_FINAL:
+            output.write("final ")
+        if accessFlags & ACC_VOLATILE:
+            output.write("volitile ")
+        if accessFlags & ACC_TRANSIENT:
+            output.write("transient ")
+        output.write(constantPool[nameIndex][1] + ":" + constantPool[descriptorIndex][1] + "\n")
+        output.write(("\t%s\n" * 3) % (h1, h2, h3))
+        (h, attributeCount) = readShort()
+        output.write("\t; attributes; size=%i\n" % attributeCount)
+        output.write("\t" + h + "\n")
+        for i in range(attributeCount):
+            readAttribute("\t\t")
+        output.write("\t\n")
     
 
 if __name__ == "__main__":
