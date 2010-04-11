18fd60109141b8b8f7a2b5cf4aa7b7eb3b470edc
Josh Wolfe <thejoshwolfe@gmail.com>
2010-04-11 16:40:11 -0700

    tests for fields

---
+++ test/call/oop/FieldsCall.java
@@ -0,0 +1,18 @@
+public class FieldsCall
+{
+    public static void main(String[] args)
+    {
+        boolean pass = true;
+        pass &= Fields.getFive() == 5;
+        pass &= Fields.ten == 10;
+        pass &= Fields.getObject() != null;
+        pass &= Fields.getThing() == null;
+        pass &= Fields.setThing("asdf");
+        pass &= Fields.getThing() == "asdf";
+        pass &= Fields.setThisThing("fdsa");
+        pass &= Fields.getThisThing() == "fdsa";
+        if (!pass)
+            System.out.println("*** FAIL");
+        System.out.println(FunctionInvocation.pass());
+    }
+}
